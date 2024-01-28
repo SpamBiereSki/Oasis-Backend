70737b3d84b07524aaa4373549aa2721b00b1204
Marie Sarbiewski <marie.sarbiewski@gmail.com>
2024-01-28 09:53:21 +0100

    Adding basic files

---
+++ feed/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.timezone import now
+
+class Post(models.Model):
+    title = models.CharField(max_length=1000)
+    post_content = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField(default=now, editable=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    comment_content = models.CharField(max_length=1000)
+    related_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(default=now, editable=False)
+    
+    def __str__(self) -> str:
+        return self.comment_content
